9642a006a1634bd749180d67aa64b54f7e98f804
Junkins <wanwansiba3@gmail.com>
2016-08-02 13:43:36 +0900

    テスト用のbootstrap.php作成

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\I18n\I18n;
+use Cake\Log\Log;
+use Cake\Routing\DispatcherFactory;
+
+require_once 'vendor/autoload.php';
