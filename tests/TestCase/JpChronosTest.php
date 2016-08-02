b9a05bdfcb7f3dbf7c0474c31a2c0a6af419156e
Junkins <wanwansiba3@gmail.com>
2016-08-02 13:38:28 +0900

    テスト用のクラスの作成

---
+++ tests/TestCase/JpChronosTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JpChronos\Test\TestCase;
+
+use Cake\TestSuite\TestCase;
+use JpChronos\JpChronos;
+
+/**
+ * JpChronos Test
+ *
+ */
+class JpChronosTest extends TestCase
+{
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
